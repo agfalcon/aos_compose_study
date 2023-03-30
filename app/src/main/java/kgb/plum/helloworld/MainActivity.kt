3e669ecfa59c92cf53bbf937c369247bb2785646
agfalcon <agfalcon12@gmail.com>
2023-03-30 16:19:16 +0900

    Component Button Study Complete

--- app/src/main/java/kgb/plum/helloworld/MainActivity.kt
+++ app/src/main/java/kgb/plum/helloworld/MainActivity.kt
@@ -1,12 +1,16 @@
 package kgb.plum.helloworld
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -25,13 +29,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             HelloWorldTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                ButtonExample(onButtonClicked = {
+                    Toast.makeText(this, "Send clicked.", Toast.LENGTH_SHORT).show()
+                })
             }
         }
     }
@@ -40,7 +40,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     Text(
-        modifier = Modifier.size(300.dp).padding(12.dp),
+        modifier = Modifier
+            .size(300.dp)
+            .padding(12.dp),
         color = Orange,
         text = "Hello $name\nHello $name\nHello $name",
         fontSize = 30.sp,
@@ -53,10 +55,29 @@ fun Greeting(name: String) {
     )
 }
 
+@Composable
+fun ButtonExample(onButtonClicked: () -> Unit) {
+    Button(
+        onClick = onButtonClicked,
+        enabled = true,
+        border = BorderStroke(3.dp, Color.Green),
+        shape = CircleShape,
+        contentPadding = PaddingValues(20.dp)
+        ){
+        Icon(
+            modifier = Modifier.size(24.dp),
+            imageVector = Icons.Filled.Send,
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.size(ButtonDefaults.IconSpacing))
+        Text(text = "Send")
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     HelloWorldTheme {
-        Greeting("Android")
+        ButtonExample(onButtonClicked = {})
     }
 }
